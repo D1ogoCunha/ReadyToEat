487badeab487b27ea092a7bd12d2769216ff052a
Bruno Carvalho <8220111@estg.ipp.pt>
2025-04-25 19:38:31 +0100

    refactor(pendingRequest): some adjustments in pending restaurant requests

--- routes/order.js
+++ routes/order.js
@@ -3,18 +3,8 @@ const router = express.Router();
 const orderController = require("../controllers/orderController");
 const authController = require("../controllers/authController");
 
-/*router.get("/", authController.verifyLoginUser, orderController.renderOrderPage);
-router.get("/phone", authController.verifyLoginUser, orderController.renderPhoneOrderPage);
-router.post("/phone", authController.verifyLoginUser, orderController.createPhoneOrder);
-router.get("/history", authController.verifyLoginUser, orderController.getOrderHistory);
-*/
-
-router.use(authController.verifyLoginUser); // aplica a todas as rotas deste router
-
-//router.get('/', orderController.renderOrderPage);
-router.get('/phone', orderController.renderPhoneOrderPage);
-router.post('/phone', orderController.createPhoneOrder);
-router.get('/', orderController.getOrderHistory);
-
+router.get('/phone', authController.verifyLoginUser, orderController.renderPhoneOrderPage);
+router.post('/phone', authController.verifyLoginUser, orderController.createPhoneOrder);
+router.get('/', authController.verifyLoginUser, orderController.getOrderHistory);
 
 module.exports = router;
