a7a259684a73ef8d3db74afd37f9b78431775dbc
Tomás Nunes <8220107@estg.ipp.pt>
2025-05-12 13:28:03 +0100

    feature(shopping-cart): add shopping cart page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { RestaurantsComponent } from './pages/restaurants/restaurants.component'
 import { RestaurantMenusComponent } from './pages/restaurant-menus/restaurant-menus.component';
 import { LoginComponent } from './auth/login/login.component';
 import { DishDetailsComponent } from './pages/dish-details/dish-details.component';
+import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 
 export const routes: Routes = [
   { path: 'dishes', component: DishesComponent },
   { path: 'dishes/:dishId/details', component: DishDetailsComponent },
   { path: 'restaurants', component: RestaurantsComponent },
+  { path: 'shopping-cart', component: ShoppingCartComponent },
   {
     path: 'restaurants/:restaurantId/menus',
     component: RestaurantMenusComponent,
---
+++ client/src/app/pages/shopping-cart/shopping-cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+@Component({
+  selector: 'app-shopping-cart',
+  imports: [NavbarComponent],
+  templateUrl: './shopping-cart.component.html',
+  styleUrl: './shopping-cart.component.css'
+})
+export class ShoppingCartComponent {
+
+}
