0c8b65f45baba2daad96ac676c62998d271ceed8
Tomás Nunes <8220107@estg.ipp.pt>
2025-05-28 16:12:41 +0100

    feature(editProfile): css improve and adjustment

--- client/src/app/pages/user/profile-chart/profile-chart.component.ts
+++ client/src/app/pages/user/profile-chart/profile-chart.component.ts
@@ -3,7 +3,7 @@ import { ProfileService } from '../../../services/profile.service';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from '../../../components/navbar/navbar.component'; 
 import { RouterModule } from '@angular/router';
-
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-profile-chart',
@@ -15,6 +15,7 @@ import { RouterModule } from '@angular/router';
 export class ProfileChartComponent implements OnInit {
   chartData: any;
   errorMessage = '';
+  chart: any;
 
   constructor(private profileService: ProfileService) {}
 
@@ -22,10 +23,45 @@ export class ProfileChartComponent implements OnInit {
     this.profileService.getChart().subscribe({
       next: (data) => {
         this.chartData = data;
+        this.createChart();
       },
       error: () => {
         this.errorMessage = 'Erro ao carregar estatísticas.';
       }
     });
   }
+
+createChart() {
+  if (this.chart) {
+    this.chart.destroy();
+  }
+
+  const labels = this.chartData.map((item: any) => item.name);
+  const values = this.chartData.map((item: any) => item.count);
+
+  const ctx = document.getElementById('mostOrderedDishesChart') as HTMLCanvasElement;
+  if (ctx) {
+    this.chart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: 'Pedidos',
+          data: values,
+          backgroundColor: 'rgba(54, 162, 235, 0.5)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+}
 }
