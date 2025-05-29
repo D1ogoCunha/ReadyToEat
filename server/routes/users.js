1de26f5b4e90f93edccf50011f59bc2a4951adcb
Diogo Cunha <8220100@estg.ipp.pt>
2025-05-29 16:50:40 +0100

    feature(swagger): added swagger comments

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,4 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const authController = require("../controllers/authController");
 const userController = require("../controllers/userController");
@@ -15,30 +15,139 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
+router.get(
+  "/profile/security",
+  authController.verifyLoginUser,
+  userController.renderSecurityProfilePage
+);
+router.post(
+  "/profile/security",
+  authController.verifyLoginUser,
+  userController.updatePassword
+);
 
-router.get("/profile/security", authController.verifyLoginUser, userController.renderSecurityProfilePage);
-router.post("/profile/security", authController.verifyLoginUser, userController.updatePassword);
-
-router.get("/profile/edit", authController.verifyLoginUser, userController.renderProfilePage);
+router.get(
+  "/profile/edit",
+  authController.verifyLoginUser,
+  userController.renderProfilePage
+);
 router.post(
   "/profile/edit",
   authController.verifyLoginUser,
-  upload.single('profileImage'),
+  upload.single("profileImage"),
   userController.updateProfile
 );
 
-router.get("/profile/chart", authController.verifyLoginUser, userController.renderChartPage);
-router.post("/profile/chart", authController.verifyLoginUser, userController.getMostOrderedDishes);
+router.get(
+  "/profile/chart",
+  authController.verifyLoginUser,
+  userController.renderChartPage
+);
+router.post(
+  "/profile/chart",
+  authController.verifyLoginUser,
+  userController.getMostOrderedDishes
+);
 
-router.get("/restaurants", authController.verifyLoginUser, userController.getRestaurants);
-router.get("/restaurants/:restaurantId", authController.verifyLoginUser, userController.getRestaurantesById);
+/**
+ * @swagger
+ * /users/restaurants:
+ *   get:
+ *     summary: Get all valid restaurants
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of restaurants
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *       401:
+ *         description: Unauthorized
+ */
+router.get(
+  "/restaurants",
+  authController.verifyLoginUser,
+  userController.getRestaurants
+);
 
-router.get("/restaurants/:restaurantId/menus", authController.verifyLoginUser, userController.getMenusByRestaurantId);
+/**
+ * @swagger
+ * /users/restaurants/{restaurantId}:
+ *   get:
+ *     summary: Get restaurant details by ID
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: restaurantId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Restaurant ID
+ *     responses:
+ *       200:
+ *         description: Restaurant data
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Restaurant not found
+ */
+router.get(
+  "/restaurants/:restaurantId",
+  authController.verifyLoginUser,
+  userController.getRestaurantesById
+);
 
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/**
+ * @swagger
+ * /users/restaurants/{restaurantId}/menus:
+ *   get:
+ *     summary: Get menus for a restaurant
+ *     tags:
+ *       - Users
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: restaurantId
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Restaurant ID
+ *     responses:
+ *       200:
+ *         description: List of menus
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Menus not found
+ */
+router.get(
+  "/restaurants/:restaurantId/menus",
+  authController.verifyLoginUser,
+  userController.getMenusByRestaurantId
+);
+
+router.get("/", function (req, res, next) {
+  res.send("respond with a resource");
 });
-
-
 
 module.exports = router;
